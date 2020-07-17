c8d081e20acdef3f72367ce159a6b5293261e41f
Adam Ouellette <adamouellette57@gmail.com>
2020-07-17 11:24:45 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -10,21 +10,8 @@ else
   exit 1
 fi
 
-
-# copy Vs-Code files
-cp  $HOME/Library/Application\ 
-Support/Code/User/{keybindings.json,settings.json,spellright.dict} .
-
-# copy snippets folder
-cp -r $HOME/Library/Application\ Support/Code/User/snippets .
-
-# copy list of extensions that currently installed
-code --list-extensions --show-versions >> ../../vscode/extensions.txt
-
 # copy other dot files 
 cp  $HOME/{.zshrc,.vimrc} .
-
-
 
 # Check git status
 gs="$(git status | grep -i "modified")"
